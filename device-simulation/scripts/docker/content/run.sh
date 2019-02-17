c31cc578b32c7bcb7118dd693363cbdb3e3e59be
Ruslan Kadyrov <kadyrov96@mail.ru>
2019-02-17 14:00:25 +0300

    Removed unnecessary projects

--- device-simulation/scripts/docker/content/run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# Copyright (c) Microsoft. All rights reserved.
-
-cd /app/
-
-cd webservice && dotnet Microsoft.Azure.IoTSolutions.DeviceSimulation.WebService.dll
--- storage-adapter/WebService/Wrappers/IKeyGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿// Copyright (c) Microsoft. All rights reserved.
-
-namespace Microsoft.Azure.IoTSolutions.StorageAdapter.WebService.Wrappers
-{
-    public interface IKeyGenerator
-    {
-        string Generate();
-    }
-}
